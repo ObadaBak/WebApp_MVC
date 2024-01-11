d674ee45bff91757641158f7c37b748e9acccb68
OBADA BAKFALOUNI <obada5781@gmail.com>
2024-01-11 19:16:38 +0300

    Add project files.

---
+++ WebProgrammıng/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebProgrammıng.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
